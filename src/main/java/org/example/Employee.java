efaded31b71e9e3672d4b4a53804bcbab0dec31b
NiklasRestory <niklas.cullberg@restory.ai>
2023-11-14 11:31:13 +0100

    Added Employee class and EmployeeHandler

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,62 @@
+package org.example;
+
+public class Employee {
+    private String name;
+    private int age;
+    private String occupation;
+    private int salary;
+    private String company;
+
+    public Employee(String name, int age, String occupation, int salary, String company) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.salary = salary;
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getDescription() {
+        return getName() + "," + getAge() + "," +
+                getOccupation() + "," + getSalary() + "," + getCompany();
+    }
+}
---
+++ src/main/java/org/example/EmployeeHandler.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeeHandler {
+    ArrayList<Employee> employeeList = new ArrayList<>();
+
+    public EmployeeHandler() {
+        employeeList.add(new Employee("Jim", 23, "Artist", 200000, "Jim CO"));
+        employeeList.add(new Employee("Jake", 26, "Painter", 5000, "Jake CO"));
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    public void mainMenu() {
+        while (true)
+        {
+            System.out.println("What would you like to do?");
+            System.out.println("1. View Employees");
+            System.out.println("2. Add Employee");
+            System.out.println("3. Remove Employee");
+            System.out.println("4. Save.");
+            System.out.println("0. Quit");
+
+            // We don't need to create a scanner and such in C#, just Console.ReadLine() to get user input.
+            String input = scanner.nextLine();
+
+            // switch-case statements are the same.
+            switch (input)
+            {
+                case "1":
+                    ViewEmployees();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    Save();
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Not applicable");
+                    break;
+            }
+        }
+    }
+
+    private void Save() {
+    }
+
+    private void ViewEmployees() {
+        for (Employee employee : employeeList) {
+            System.out.println(employee.getDescription());
+        }
+    }
+}
